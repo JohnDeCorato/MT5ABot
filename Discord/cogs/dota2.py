ea793f2bc615fe8a28e4e90db35e841318813bd7
JohnDeCorato <jd537@cornell.edu>
2016-05-02 00:55:47 -0400

    Match parsing from Web API added
    Implemented match parsing

--- Discord/cogs/dota2.py
+++ Discord/cogs/dota2.py
@@ -16,6 +16,16 @@ class Dota2:
     def __init__(self, bot):
         self.bot = bot
         self.steam_api = steamapi.SteamAPI(bot.steam_api_key)
+        with open("Dota/heroes.json", 'r') as f:
+            self.heroes = json.load(f)['result']['heroes']
+        with open("Dota/items.json", 'r') as f:
+            self.items = json.load(f)['result']['items']
+        with open("Dota/lobbies.json", 'r') as f:
+            self.lobbies = json.load(f)['lobbies']
+        with open("Dota/modes.json", 'r') as f:
+            self.modes = json.load(f)['modes']
+        with open("Dota/regions.json", 'r') as f:
+            self.regions = json.load(f)['regions']
 
     @commands.command(hidden=True)
     @checks.is_owner()
@@ -24,6 +34,8 @@ class Dota2:
 
         with open("Dota/heroes.json", 'w') as f:
             json.dump(heroes, f, ensure_ascii=True, indent=4)
+
+        self.heroes = heroes
 
     @commands.command(hidden=True)
     @checks.is_owner()
@@ -80,11 +92,106 @@ class Dota2:
 
         return latest_match
 
+    def get_hero_name(self, i):
+        for hero in self.heroes:
+            if hero['id'] == i:
+                return hero['localized_name']
+        return 'Unknown Hero'
+
+    def get_item_name(self, i):
+        for item in self.items:
+            if item['id'] == i:
+                return item['localized_name']
+        return 'Unknown Item'
+
+    def get_lobby_name(self, i):
+        for lobby in self.lobbies:
+            if lobby['id'] == i:
+                return lobby['name']
+        return 'Unknown Lobby Type'
+
+    def get_mode_name(self, i):
+        for mode in self.modes:
+            if mode['id'] == i:
+                return mode['name']
+        return 'Unknown Game Mode'
+
+    def get_region_name(self, i):
+        for region in self.regions:
+            if region['id'] == i:
+                return region['name']
+        return 'Unknown Matchmaking Region'
+
+    def get_game_length(self, duration):
+        minutes = int(duration / 60)
+        seconds = int(duration % 60)
+        return "{0}:{1}".format(minutes, str(seconds).zfill(2))
+
+    def get_player_blurb(self, player):
+        dota_id = player['account_id']
+
+        name = None
+        for server in self.bot.servers:
+            for member in server.members:
+                steam_ids = self.bot.steam_info.get(member.id)
+                if steam_ids is not None:
+                    for steam_id in steam_ids:
+                        if dota_id == int(steam_id) - steamapi.ID.STEAM_TO_DOTA_CONSTANT:
+                            name = member.name
+                            break
+
+        if name is None:
+            return None
+
+        hero_name = self.get_hero_name(player['hero_id'])
+        return ("__Player -- {0}__\n"
+                "Hero -- {1}\n"
+                "Level -- {2}\n"
+                "K/D/A -- {3}/{4}/{5}\n"
+                "GPM -- {6}\n\n".format(name, hero_name, player['level'], player['kills'],
+                                    player['deaths'], player['assists'], player['gold_per_min']))
+
     async def parse_match(self, match):
-        match_info = self.steam_api.get_match_details(match['match_id'])
+        result = self.steam_api.get_match_details(match['match_id'])
+        try:
+            match_info = result['result']
+        except KeyError:
+            print(result)
+            await self.bot.say("The Dota 2 Web API is down. Please try again later.")
+
+        lobby_name = self.get_lobby_name(match_info['lobby_type'])
+        mode_name = self.get_mode_name(match_info['game_mode'])
+        region_name = self.get_region_name(match_info['cluster'])
+        game_length = self.get_game_length(match_info['duration'])
+        winning_team = "Radiant" if match_info['radiant_win'] else "Dire"
 
         match_string = ''
+        match_string += "Lobby Type -- {0}\n".format(lobby_name)
+        match_string += "Game Mode -- {0}\n".format(mode_name)
+        match_string += "Region -- {0}\n".format(region_name)
+        match_string += "Duration -- {0}\n".format(game_length)
+        match_string += "Winning Team -- {0}\n\n".format(winning_team)
 
+        match_string += "<http://www.dotabuff.com/matches/{0}>\n\n".format(match['match_id'])
+
+        player_count = 0
+        print_rad = False
+        print_dir = False
+
+        for player in match_info['players']:
+            player_count += 1
+            player_blurb = self.get_player_blurb(player)
+
+            if player_blurb is not None:
+                if not print_rad and player_count <= 5:
+                    match_string += "__**Radiant Team**__\n\n"
+                    print_rad = True
+                if not print_dir and player_count > 5:
+                    match_string += "__**Dire Team**__\n\n"
+                    print_dir = True
+                match_string += player_blurb
+
+        await self.bot.say(match_string)
 
     @commands.command(pass_context=True)
     async def last_match(self, ctx, *, member: discord.Member=None):
@@ -104,6 +211,7 @@ class Dota2:
             return
 
         match = self.get_latest_match_from_list(steam_ids)
+        await self.parse_match(match)
 
 
 def setup(bot):
